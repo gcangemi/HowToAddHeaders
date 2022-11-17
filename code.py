f02bfcd507352a7ade2fcbf28323e53962bdd75a
gcangemi <102766169+gcangemi@users.noreply.github.com>
2022-11-17 17:52:10 +0100

    Update code.py

--- code.py
+++ code.py
@@ -6,13 +6,12 @@ Created on Thu Nov 17 17:16:14 2022
 """
 import pandas as pd
 dt_rettificato = pd.read_excel(r"C:\Project\AAA\RwoH.xlsx")
-#dt_rettificato.columns = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"]
 
-print(len(dt_rettificato.columns)) #output: 15 (15 colonne - da "a" -> "o")
+print(len(dt_rettificato.columns)) #output: 15 (15 columns - from "a" to "o")
 len_df = len(dt_rettificato.columns)
 
-df_alf = pd.read_excel(r"C:\Project\AAA\array.xlsx")
-print(len(df_alf))
+df_alf = pd.read_excel(r"C:\YourProject\DataFrame_alphabetic.xlsx")
+print(len(df_alf)) #output: 50 (50 columns - from "a" to "ay"
 len_alf = len(df_alf)
         
 letters = list()        
